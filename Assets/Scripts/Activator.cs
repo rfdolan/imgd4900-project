12af910898d477efaa7759081c152b57a9e6058d
AlejandraGarza42 <agarza@wpi.edu>
2019-09-05 21:02:16 -0400

    Meta Files from new assests
    looks they were not committed correctly last time

---
+++ Assets/Scripts/Activator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public bool enter = true;
+    public bool exit = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enter)
+        {
+            Debug.Log("activating");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (exit)
+        {
+            Debug.Log("deactivating");
+        }
+    }
+
+}

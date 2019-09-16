946092ab01bc85813d2d4bc389d7078d2b475f74
AlejandraGarza42 <agarza@wpi.edu>
2019-09-16 19:46:48 -0400

    Fixed Door Functionality
    Changed dimension for cylinder - part of puzzle

---
+++ Assets/Scripts/ActivatorDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatorDoor : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        // anim.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OpenDoor()
+    {
+        Debug.Log("second door opened");
+        anim.SetTrigger("DoorOpen");
+    }
+
+
+    private void pauseAnimationEvent()
+    {
+        anim.enabled = false;
+    }
+}

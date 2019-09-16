8e123255a62849c5a1b2f941aafe94dde7fbb4d7
rfdolan <rfdolan@wpi.edu>
2019-09-16 14:21:12 -0400

    Modified picking up again
    Now the object is dropped if it collides with a wall, which fixes my issue for now. Also, you can now move it vertically while holding!

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -11,12 +11,16 @@ public class Pickup : MonoBehaviour
 {
 
     public Transform onHand;
-    private Rigidbody rb;
+    public Rigidbody rbOnHand;
     public Camera cam;
+    public Material highlightMat;
+    private Rigidbody rb;
     private PlayerController dimensionScript;
     private bool handsFull;
     private GameObject objHolding;
-    private bool allowedToDrop;
+    private bool allowedToDrop = true;
+    private GameObject objectSeen;
+    private Material objectSeenMat;
 
     void Start() 
     {
@@ -46,28 +50,70 @@ public class Pickup : MonoBehaviour
             }
         }
         //TODO make this less janky when I'm fresh
+        // One main issue is that our raycast hits the activator range before it hits the activator, which causes problems.
+        // ex highlighting the activator when we're looking at the range and letting us pick it up by the range.
         if(handsFull)
         {
+            Transform ourTransform = objHolding.GetComponent<Transform>();
+            
             // If we hit something within holding distance that isn't the object, then we want it to freeze.
             if(Physics.Raycast(ray, out hit, 2) && (hit.transform != objHolding.GetComponent<Transform>()))
             {
-                rb = objHolding.GetComponent<Rigidbody>();
+                //rb = objHolding.GetComponent<Rigidbody>();
                 //rb.useGravity = false;
                 // It no longer sticks to the holder.
-                objHolding.GetComponent<Transform>().parent = null;
+                //objHolding.GetComponent<Transform>().parent = null;
                 // It just floats in mid air until we change it.
-                rb.constraints = RigidbodyConstraints.FreezeAll;
+                //rb.constraints = RigidbodyConstraints.FreezeAll;
                 // The player is not allowed to drop the object since they can't see it (it could be out of bounds).
                 allowedToDrop = false;
             }
             else
             {
-                rb = objHolding.GetComponent<Rigidbody>();
-                objHolding.GetComponent<Transform>().position = onHand.transform.position;
-                objHolding.GetComponent<Transform>().parent = this.transform;
+                //rb = objHolding.GetComponent<Rigidbody>();
+                ourTransform.position = onHand.transform.position;
+                //objHolding.GetComponent<Transform>().parent = this.transform;
                 rb.constraints = RigidbodyConstraints.FreezeRotation;
                 // Let them drop it. because they can definitely see it.
                 allowedToDrop = true;
+            }
+            
+            // Clamp vertical movement
+            if(ourTransform.position.y <= 0.5f)
+            {
+                ourTransform.position = new Vector3(ourTransform.position.x, 0.5f, ourTransform.position.z);
+            }
+            if(ourTransform.position.y >= 2.0f)
+            {
+                ourTransform.position = new Vector3(ourTransform.position.x, 2.0f, ourTransform.position.z);
+            }
+        }
+        
+        else
+        {
+            // We are seeing a pick upable object
+            if(Physics.Raycast(ray, out hit, 2))
+            {
+                Transform objectHit = hit.transform;
+                if(objectHit.tag == "Liftable")
+                {
+                    //Debug.Log("Highlight");
+                    // Store the object and it's material so we can change it back when we stop looking.
+                    objectSeen = objectHit.gameObject;
+                    if(!objectSeen.GetComponent<Renderer>().material.name.Contains(highlightMat.name))
+                    {
+                        objectSeenMat = objectSeen.GetComponent<Renderer>().material;
+                    }
+                    objectSeen.GetComponent<Renderer>().material = highlightMat;
+                }
+            }
+            // If we just stopped looking at something, unhighlight.
+            else if(objectSeen != null)
+            {
+                //Debug.Log("Un-Highlight");
+                objectSeen.GetComponent<Renderer>().material = objectSeenMat;
+                objectSeen = null;
+                objectSeenMat = null;
             }
         }
     }
@@ -82,14 +128,15 @@ public class Pickup : MonoBehaviour
             //Debug.Log("I am going to pick this up");
             rb.useGravity = false;
             //rb.constraints = RigidbodyConstraints.FreezeAll;
-            objHit.position = onHand.transform.position;
+            objHit.position = onHand.position;
             objHit.parent = this.transform;
         }
         handsFull = !handsFull;
     }
 
-    private void dropObject()
+    public void dropObject()
     {
+        Debug.Log("Drop it mr.");
         if(dimensionScript.dimension == 1)
         {
             objHolding.GetComponent<Transform>().parent = GameObject.FindWithTag("HumanDim").GetComponent<Transform>();

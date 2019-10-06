381639900a2f71ac3fd27a8be34622c745ac44ad
rfdolan <rfdolan@wpi.edu>
2019-10-06 11:15:37 -0400

    Add garbo to project

---
+++ Assets/Scripts/MenuHover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuHover : MonoBehaviour
+{
+
+    Text ourtext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ourtext = this.GetComponent<UnityEngine.UI.Text>();
+        ourtext.material.color = Color.black;
+        
+    }
+    public void OnPointerEnter()
+    {
+        ourtext.material.color = Color.red;
+        ourtext.fontStyle = FontStyle.Bold;
+        ourtext.fontSize = 24;
+    }
+    public void OnPointerExit()
+    {
+        ourtext.material.color = Color.black;
+        ourtext.fontStyle = FontStyle.Normal;
+        ourtext.fontSize = 12;
+    }
+
+}
